b071976d80cd6a3b454113f8d27a22aabfab96ba
duykhanh <khanhtd@m2w.vn>
2022-03-04 19:52:36 +0700

    fix

--- Chichifilm/src/config/db/index.js
+++ Chichifilm/src/config/db/index.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-async function connect(){
-    try{
-        await mongoose.connect('mongodb+srv://kimmchi:1234561@cluster0.tyyyq.mongodb.net/filmchichi?retryWrites=true&w=majority', {
+async function connect() {
+    try {
+        await mongoose.connect('mongodb://localhost:27017/filmcc', {
             useNewUrlParser: true,
             useUnifiedTopology: true,
-         });
-        console.log("Connect successfully!!!");
-    }catch (error) {
-        console.log("Connect failure!!!");
+        });
+        console.log('Connect successfully!!!');
+    } catch (error) {
+        console.log('Connect failure!!!');
     }
-} 
+}
 module.exports = { connect };
--- Chichifilm/src/controller/adminController.js
+++ Chichifilm/src/controller/adminController.js
@@ -1,102 +1,137 @@
-const Film = require('./model/films');
-const { mutipleMongooseToObject } = require('../ulti/mongoose');
-const { mongooseToObject } = require('../ulti/mongoose');
+const Film = require('../model/films');
+const { mutipleMongooseToObject } = require('../utils/mongoose');
+const { mongooseToObject } = require('../utils/mongoose');
 
-class AdminController{
+class AdminController {
     admin(req, res) {
-        const title = "Admin";
-        res.render('admin/actions', {title, login: true})
+        const title = 'Admin';
+        return res.render('admin/actions', { title, login: true });
     }
 
     addfilms(req, res) {
-        const title = "Admin";
-        res.render('admin/addfilms', {title, login: true})
+        const title = 'Admin';
+        return res.render('admin/addfilms', { title, login: true });
     }
 
     addfilms_post(req, res, next) {
-        const title = "Add films";
-        const {ten, theloai, noidung, link} = req.body;
-        Film.findOne({ten: ten})
-        .then(data => {
-            if (data) {
-                const msg = "Cảnh báo: phim này đã được đăng tải trước đó!!!";
-                res.render('admin/addfilms', {title, login: true, msg})
-            } else {
-                    if (noidung === ""){
-                    const noidung = 'Chưa cập nhật';
-                    const newFilms = new Film({ten: ten, theloai: theloai, noidung: noidung, link: link});
-                    newFilms.save();
-                    res.redirect('/')
-                    } else {
-                        const newFilms = new Film({ten: ten, theloai: theloai, noidung: noidung, link: link});
+        const title = 'Add films';
+        const { ten, theloai, noidung, link } = req.body;
+        Film.findOne({ ten: ten })
+            .then((data) => {
+                if (data) {
+                    const msg =
+                        'Cảnh báo: phim này đã được đăng tải trước đó!!!';
+                    return res.render('admin/addfilms', {
+                        title,
+                        login: true,
+                        msg,
+                    });
+                } else {
+                    if (noidung === '') {
+                        const noidung = 'Chưa cập nhật';
+                        const newFilms = new Film({
+                            ten: ten,
+                            theloai: theloai,
+                            noidung: noidung,
+                            link: link,
+                        });
                         newFilms.save();
-                        res.redirect('/')
+                        return res.redirect('/');
+                    } else {
+                        const newFilms = new Film({
+                            ten: ten,
+                            theloai: theloai,
+                            noidung: noidung,
+                            link: link,
+                        });
+                        newFilms.save();
+                        return res.redirect('/');
                     }
-            }
-        })
-        .catch(next);
+                }
+            })
+            .catch(next);
     }
 
     deletefilms(req, res, next) {
         const title = 'Xóa phim';
-            var page = req.query.page || 1;
-            var perpage = 8;
-            Film.find({})
-            .sort({date: 1})
-            .skip((perpage*page)-perpage)
+        var page = req.query.page || 1;
+        var perpage = 8;
+        Film.find({})
+            .sort({ date: 1 })
+            .skip(perpage * page - perpage)
             .limit(perpage)
-            .then(theMovies =>{
-                Film.countDocuments({})
-                .then(num=>{
-                    res.render('admin/deletefilm', {title, delete: true, login:true, admin:true, 
-                    theMovies: mutipleMongooseToObject(theMovies), num, page, perpage });
-                }) 
+            .then((theMovies) => {
+                Film.countDocuments({}).then((num) => {
+                    return res.render('admin/deletefilm', {
+                        title,
+                        delete: true,
+                        login: true,
+                        admin: true,
+                        theMovies: mutipleMongooseToObject(theMovies),
+                        num,
+                        page,
+                        perpage,
+                    });
+                });
             })
-            .catch(next);           
+            .catch(next);
     }
 
     deletefilms_id(req, res, next) {
-        Film.deleteMany({_id: req.params.id})
-        .then()
-        res.redirect('/admin/deletefilm');
+        Film.deleteMany({ _id: req.params.id }).then();
+        return res.redirect('/admin/deletefilm');
     }
 
     updatefilms(req, res, next) {
         const title = 'Sửa phim';
-            var page = req.query.page || 1;
-            var perpage = 8;
-            Film.find({})
-            .sort({date: 1})
-            .skip((perpage*page)-perpage)
+        var page = req.query.page || 1;
+        var perpage = 8;
+        Film.find({})
+            .sort({ date: 1 })
+            .skip(perpage * page - perpage)
             .limit(perpage)
-            .then(theMovies =>{
-                Film.countDocuments({})
-                .then(num=>{
-                    res.render('admin/updatefilm', {title, login:true, admin:true, 
-                    theMovies: mutipleMongooseToObject(theMovies), num, page, perpage });
-                }) 
+            .then((theMovies) => {
+                Film.countDocuments({}).then((num) => {
+                    return res.render('admin/updatefilm', {
+                        title,
+                        login: true,
+                        admin: true,
+                        theMovies: mutipleMongooseToObject(theMovies),
+                        num,
+                        page,
+                        perpage,
+                    });
+                });
             })
-            .catch(next);           
+            .catch(next);
     }
 
     updatefilms_id(req, res, next) {
         const title = 'Sửa phim';
-        Film.findOne({_id: req.params.id})
-        .then(data=>{
-            res.render('admin/updateid', {title, login: true, data: mongooseToObject(data)})
-        })
-        .catch(next)
+        Film.findOne({ _id: req.params.id })
+            .then((data) => {
+                return res.render('admin/updateid', {
+                    title,
+                    login: true,
+                    data: mongooseToObject(data),
+                });
+            })
+            .catch(next);
     }
 
     updatenow(req, res, next) {
         const title = 'Sửa phim';
-        Film.updateOne({_id: req.params.id}, req.body)
-        .then(() => 
-        Film.findOne({_id: req.params.id})
-        .then(data=>{
-            res.render('admin/updateid', {title, login: true, data: mongooseToObject(data)})
-        }))
-        .catch(next);
+        Film.updateOne({ _id: req.params.id }, req.body)
+            .then(() =>
+                Film.findOne({ _id: req.params.id }).then((data) => {
+                    return res.render('admin/updateid', {
+                        title,
+                        login: true,
+                        data: mongooseToObject(data),
+                    });
+                })
+            )
+            .catch(next);
     }
 }
 
